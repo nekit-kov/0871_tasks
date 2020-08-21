214379b36ec48cdb4735a13aef5b3484b78afbed
nekit-kov <69851557+nekit-kov@users.noreply.github.com>
2020-08-21 09:46:16 +0300

    Create Main.java

---
+++ №2.3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int fakt10=1;
+        for (int i=1;i<=10;i++)
+            fakt10*=i;
+        System.out.println(fakt10);
+    }
+}
