3013c4c8a491d9e85a1e68fe5b9e314f662e8d8a
nekit-kov <69851557+nekit-kov@users.noreply.github.com>
2020-08-21 08:58:37 +0300

    Create Main.java

---
+++ №1.5/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float TC = scan.nextFloat();
+        System.out.println(TC*9/5+32);
+        }
+    }
