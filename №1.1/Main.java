6fea5857590b071dd457a3b7f0d9606df6e3cd2a
nekit-kov <69851557+nekit-kov@users.noreply.github.com>
2020-08-21 08:54:30 +0300

    Create Main.java

---
+++ №1.1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ivan";
+        int age = 29;
+        String city = "Moscow";
+        System.out.println(name+" "+city+" "+age);
+    }
+}
