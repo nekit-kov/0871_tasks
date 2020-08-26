486dbae3b82ae781dce2cbd0089716c48fca0357
nekit-kov <69851557+nekit-kov@users.noreply.github.com>
2020-08-26 23:54:08 +0300

    Create Main.java

---
+++ №2.8/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+        public static void main(String[] args) throws Exception {
+                for (int i = 1; i<=10; i++){
+                        for (int a = 0; a<i; a++){
+                                System.out.print("8");
+                        }
+                        System.out.println();
+                }
+
+        }
+}
